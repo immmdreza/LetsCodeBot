0689ac61b0e0f0deafc2f81fbabe8d8e83cc9599
immmdreza <47322990+immmdreza@users.noreply.github.com>
2020-06-28 12:43:11 +0430

    Add files via upload

---
+++ LetsCodeApp/Handlers/UpdateHandlers/ShowAbout.cs
@@ -0,0 +1,63 @@
+﻿using LetsCodeApp.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+namespace LetsCodeApp.Handlers.UpdateHandlers
+{
+    class ShowAbout : IHandler
+    {
+        public UpdateType updateType => UpdateType.Message;
+
+        public string[] textTriggers => new[] {"/about" , "/welcome"};
+
+        public TriggerType triggerType => TriggerType.Command;
+
+        public ChatType[] ChatTypes => new ChatType[] { ChatType.Group, ChatType.Supergroup };
+
+        public bool replyOnly => false;
+
+        public async Task<bool> Process(ITelegramBotClient botClient, Update update, NeededItems n)
+        {
+            switch (n.Params[0].ToLower())
+            {
+                case "/about":
+                    {
+                        var c = new GroupServices();
+
+                        var abt = await c.AboutmsgByTlId(n.ChatId);
+
+                        if (string.IsNullOrEmpty(abt))
+                        {
+                            return false;
+                        }
+
+                        await botClient.SendTextMessageAsync(n.ChatId, abt, ParseMode.Html);
+
+                        return true;
+                    }
+                case "/welcome":
+                    {
+                        var c = new GroupServices();
+
+                        var wlc = await c.WlcmsgByTlId(n.ChatId);
+
+                        if (string.IsNullOrEmpty(wlc))
+                        {
+                            return false;
+                        }
+
+                        await botClient.SendTextMessageAsync(n.ChatId, wlc);
+
+                        return true;
+                    }
+
+                default: return true;
+            }
+        }
+    }
+}
